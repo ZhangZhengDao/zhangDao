0cf2595196fa9715838781e730d52b18c0f7e969
214834 <=>
2020-01-03 10:53:28 +0800

    第二讲

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ##项目启动第一天
 创建项目
 部署在git中
+##前端框架
+bootstrap
+网址：https://www.bootcss.com/
--- src/main/java/cn/zhang/com/controller/HelloController.java
+++ src/main/java/cn/zhang/com/controller/HelloController.java
@@ -1,20 +1,15 @@
 package cn.zhang.com.controller;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String into(@RequestParam() Model model){
-        model.addAttribute("name","你好");
-        return "into";
+    public String into(){
+        return "index";
     }
 }
