946d36bc1536b26a617ff3f0a9fc4aaa77861415
214834 <=>
2020-01-03 09:57:28 +0800

    into rep

---
+++ src/main/java/cn/zhang/com/ZhangApplication.java
@@ -0,0 +1,13 @@
+package cn.zhang.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zhang/com/ZhangApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhang.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
